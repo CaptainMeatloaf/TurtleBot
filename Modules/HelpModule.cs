53c19c8e85ac25cf902f3607b7b1cbc151e51ce9
CaptainMeatloaf <github@watchingyour.tv>
2018-01-17 02:12:33 +0000

    Reworked help command

--- Modules/HelpModule.cs
+++ Modules/HelpModule.cs
@@ -6,13 +6,33 @@ namespace TurtleBot.Modules
     public class HelpModule : ModuleBase<SocketCommandContext>
     {
         [Command("help")]
-        public Task Help()
-            => ReplyAsync(
-                $"The following commands are available:\n```" +
-                "!height:      Gets the current block height\n" +
-                "!hashrate:    Gets the current global hashrate\n" +
-                "!supply:      Gets the current circulating supply of TRTL\n" +
-                "!difficulty:  Gets the current difficulty\n" + 
-                "!help:        Prints this help dialog```");
+        public async Task Help(string commandToGetHelpFor = null)
+        {
+            if(string.IsNullOrWhiteSpace(commandToGetHelpFor))
+            {
+                await ReplyAsync("Available commands: `height`, `hashrate`, `supply`, `difficulty`, `help`");
+            }
+            else
+            {
+                switch(commandToGetHelpFor.ToLowerInvariant())
+                {
+                    case "height":
+                        await ReplyAsync("`!height` Gets the current block height");
+                        return;
+                    case "hashrate":
+                        await ReplyAsync("`!hashrate` Gets the current global hashrate");
+                        return;
+                    case "supply":
+                        await ReplyAsync("`!supply` Gets the current circulating supply of TRTL");
+                        return;
+                    case "difficulty":
+                        await ReplyAsync("`!difficulty` Gets the current difficulty");
+                        return;
+                    case "help":
+                        await ReplyAsync("`!help <(optional) command>` Prints the list of commands or help for a specific command");
+                        return;
+                }
+            }
+        }
     }
 }
