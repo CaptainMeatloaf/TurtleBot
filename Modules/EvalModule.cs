fc7fe48a689c73be10e79ff3d00d2ed44d6dc795
CaptainMeatloaf <github@watchingyour.tv>
2018-06-24 23:14:26 +0100

    Removed daemon and help modules

---
+++ Modules/EvalModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+namespace TurtleBot.Modules
+{
+    [Summary("Evals commands from strings in the given context")]
+    public class EvalModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("eval")]
+        [RequireOwner]
+        public async Task Eval([Remainder] string codeToEval)
+        {
+            Script<object> evalScript = CSharpScript.Create(codeToEval, ScriptOptions.Default.WithReferences(Assembly.GetExecutingAssembly()), this.GetType());
+            await ReplyAsync((await evalScript.RunAsync(this)).ReturnValue.ToString());
+        }
+    }
+}
